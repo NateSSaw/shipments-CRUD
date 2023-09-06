a7a37f2aa0e869bd7e0fcbb0347ea35866f0d113
NateSSaw <natasemenova1806@gmail.com>
2023-09-06 23:21:47 +0000

    Deploying to gh-pages from @ NateSSaw/shipments-CRUD@cd9ebdd98785e01996414454e901820e27a469a6 🚀

---
+++ static/js/544.3f662f0a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact=self.webpackChunkreact||[]).push([[544],{544:function(e,c,n){n.r(c),n.d(c,{default:function(){return t}});var r=n(184);function t(){return(0,r.jsx)("div",{children:(0,r.jsx)("h1",{children:"Welcome. Nice to meet you here.)"})})}}}]);
+//# sourceMappingURL=544.3f662f0a.chunk.js.map
