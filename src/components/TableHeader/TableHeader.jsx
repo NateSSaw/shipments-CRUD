7e46d93dba9c7b9afa998abc1a22e151426de24b
Nataliia Semenova <natasemenova1806@gmail.com>
2023-09-12 11:44:28 +0300

    fix buttons sort

--- src/components/TableHeader/TableHeader.jsx
+++ src/components/TableHeader/TableHeader.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import './TableHeader.module.css';
+import css from './TableHeader.module.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { getShipmentsList } from 'redux/shipments/selectors';
 import { sortShipmentsData } from 'redux/sort/slice';
@@ -42,74 +42,90 @@ export const TableHeader = () => {
       <tr>
         <th>ORDERNO</th>
         <th>DELIVERYDATE</th>
-        <th
-          onClick={() => {
-            sortData('customer');
-          }}
-        >
-          <span>CUSTOMER</span>
-          <ArrowUp
+        <th>
+          <span className={css.customer}>CUSTOMER</span>
+          <button
+            className={css.svg}
+            type="button"
             onClick={() => {
               sortData('customer');
             }}
-          />
-          <ArrowDown
+          >
+            <ArrowUp />
+          </button>
+          <button
+            className={css.svg}
+            type="button"
             onClick={() => {
               sortData('customer');
             }}
-          />
+          >
+            <ArrowDown />
+          </button>
         </th>
-        <th
-          onClick={() => {
-            sortData('trackingNo');
-          }}
-        >
-          <span>TRACKINGNO</span>
-          <ArrowUp
+        <th>
+          <span className={css.trackingNo}>TRACKINGNO</span>
+          <button
+            className={css.svg}
+            type="button"
             onClick={() => {
               sortData('trackingNo');
             }}
-          />
-          <ArrowDown
+          >
+            <ArrowUp />
+          </button>
+          <button
+            className={css.svg}
+            type="button"
             onClick={() => {
               sortData('trackingNo');
             }}
-          />
+          >
+            <ArrowDown />
+          </button>
         </th>
-        <th
-          onClick={() => {
-            sortData('status');
-          }}
-        >
-          <span>STATUS</span>
-          <ArrowUp
+        <th>
+          <span className={css.status}>STATUS</span>
+          <button
+            className={css.svg}
+            type="button"
             onClick={() => {
               sortData('status');
             }}
-          />
-          <ArrowDown
+          >
+            <ArrowUp />
+          </button>
+          <button
+            className={css.svg}
+            type="button"
             onClick={() => {
               sortData('status');
             }}
-          />
+          >
+            <ArrowDown />
+          </button>
         </th>
-        <th
-          colSpan="2"
-          onClick={() => {
-            sortData('consignee');
-          }}
-        >
-          <span>CONSIGNEE</span>
-          <ArrowUp
+              <th colSpan="2">
+                  <span className={css.consignee}>CONSIGNEE</span>
+
+          <button
+            className={css.svg}
+            type="button"
             onClick={() => {
               sortData('consignee');
             }}
-          />
-          <ArrowDown
+          >
+            <ArrowUp />
+          </button>
+          <button
+            className={css.svg}
+            type="button"
             onClick={() => {
               sortData('consignee');
             }}
-          />
+          >
+            <ArrowDown />
+          </button>
         </th>
       </tr>
     </thead>
